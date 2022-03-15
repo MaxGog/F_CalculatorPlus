7d8f427956832e99d02fe4fb434722752a698841
Max <max.gog2005@outlook.com>
2022-03-15 18:01:56 +0300

    Add new app

---
+++ UWP/CalculatePage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Calculator_Plus
+{
+    public sealed partial class CalculatePage : Page
+    {
+        string leftop = "";
+        string operation = "";
+        string rightop = "";
+        public CalculatePage()
+        {
+            this.InitializeComponent();
+            foreach (UIElement c in LayoutRoot.Children)
+            {
+                if (c is Button)
+                {
+                    ((Button)c).Click += Button_Click;
+                }
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string s = (string)((Button)e.OriginalSource).Content;
+            Text_View.Text += s;
+            int number;
+            bool result = Int32.TryParse(s, out number);
+            if (result == true)
+            {
+                if (operation == "")
+                    leftop += s;
+                else
+                    rightop += s;
+            }
+            else
+            {
+                if (s == "=")
+                {
+                    Update_RightOp();
+                    //if (Text_Finish.Text != null)
+                    //    Text_Finish.Text = null;
+                    Text_Finish.Text += rightop;
+                    operation = "";
+                }
+                else if (s == "Clear")
+                {
+                    leftop = "";
+                    rightop = "";
+                    operation = "";
+                    Text_View.Text = "";
+                }
+                else
+                {
+                    if (rightop != "")
+                    {
+                        Update_RightOp();
+                        leftop = rightop;
+                        rightop = "";
+                    }
+                    operation = s;
+                }
+            }
+        }
+        private void Update_RightOp()
+        {
+            int number_one = Int32.Parse(leftop);
+            int number_two = Int32.Parse(rightop);
+            switch (operation)
+            {
+                case "+":
+                    rightop = (number_one + number_two).ToString();
+                    break;
+                case "-":
+                    rightop = (number_one - number_two).ToString();
+                    break;
+                case "×":
+                    rightop = (number_one * number_two).ToString();
+                    break;
+                case "/":
+                    rightop = (number_one / number_two).ToString();
+                    break;
+            }
+        }
+    }
+}
---
+++ UWP/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace Calculator_Plus
+{
+    public class Functionality
+    {
+        public string Icon { get; set; }
+        public string Name_str { get; set; }
+    }
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //ContentView.Navigate(typeof(CalculatePage));
+        }
+
+        private void NavPanel_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.SelectedItem == Home_Page)
+            {
+                ContentView.Content = SelectPage;
+            }
+            else if (args.SelectedItem == Calculator_Page)
+            {
+                ContentView.Navigate(typeof(CalculatePage));
+            }
+            else if (args.SelectedItem == Money_Page)
+            {
+                ContentView.Navigate(typeof(MoneyPage));
+            }
+        }
+        private void SelectPage_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Functionality selectedFunctional = (Functionality)e.ClickedItem;
+            if (selectedFunctional == calculate)
+            {
+                ContentView.Navigate(typeof(CalculatePage));
+                NavPanel.SelectedItem = Calculator_Page;
+            }
+            else if (selectedFunctional == money)
+            {
+                ContentView.Navigate(typeof(MoneyPage));
+                NavPanel.SelectedItem = Money_Page;
+            }
+        }
+    }
+}
